3f2ec5d9ce338061dc63fb211cd0b99ab23b6c74
ANDRESGAMBOAT <an.gamboat@duocuc.cl>
2024-10-31 00:01:27 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'NuevoProyecto',
+  webDir: 'www'
+};
+
+export default config;
